fe8f88430d31001be1307d24935b6a190934accc
Wang Jing <wangjingnic1993@gmail.com>
2020-10-16 11:24:28 +0800

    'Init'

---
+++ src/main/java/com/studiolo/studiolo/StudioloApplication.java
@@ -0,0 +1,13 @@
+package com.studiolo.studiolo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudioloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudioloApplication.class, args);
+	}
+
+}
