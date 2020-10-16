e46c604a37d2cc3124537edda7d0b7a9340c6043
Wang Jing <wangjingnic1993@gmail.com>
2020-10-16 14:27:57 +0800

    Init

--- src/main/java/com/studiolo/studiolo/StudioloApplication.java
+++ src/main/java/com/studiolo/studiolo/StudioloApplication.java
@@ -2,12 +2,19 @@ package com.studiolo.studiolo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class StudioloApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StudioloApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(StudioloApplication.class, args);
+    }
 
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hello Spring Boot";
+    }
 }
